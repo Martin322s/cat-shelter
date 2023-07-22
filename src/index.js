5358be62cf6b5d955d33c07477af32bd0a003b19
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-23 00:30:07 +0300

    add cat breed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ const { initViewEngine } = require('../config/hbs');
 const app = express();
 const port = 5000;
 
+const breeds = [
+    'Bombay Cat', 
+    'American Bobtail Cat', 
+    'Bengal Cat', 
+    'British Shorthair Cat', 
+    'Unknown'
+];
+
 initViewEngine(app);
 app.use('/static', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -16,6 +26,12 @@ app.get('/cats/add-cat', (req, res) => {
 
 app.get('/cats/add-breed', (req, res) => {
     res.render('addBreed');
+});
+
+app.post('/cats/add-breed', (req, res) => {
+    breeds.unshift(req.body.breed);
+    console.log(breeds);
+    res.redirect('/cats/add-cat');
 });
 
 app.listen((port), () => console.log(`Server is working at port: ${port}`));
