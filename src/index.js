cb3daf7e7c71e20cd3100027a8a68b0ee2c47348
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-22 19:52:08 +0300

    setup view engine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { initViewEngine } = require('../config/hbs');
 const app = express();
 const port = 5000;
+
+initViewEngine(app);
 
 app.listen((port), () => console.log(`Server is working at port: ${port}`));
