bbdab6e9dcd09d220d7cc6c59572e3904fc8734c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-24 10:45:19 +0300

    render all breeds

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ const express = require('express');
 const { initViewEngine } = require('../config/hbs');
 const app = express();
 const port = 5000;
-
-const breeds = {
-    breed: 'Bombay Cat',
-    brred: 'American Bobtail Cat',
-    breed: 'Bengal Cat',
-    breed: 'British Shorthair Cat',
-    breed: 'Unknown'
-};
+const breeds = require('./breeds.json');
+const fs = require('fs/promises');
 
 initViewEngine(app);
 app.use('/static', express.static('public'));
@@ -21,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cats/add-cat', (req, res) => {
-    res.render('addCat', breeds);
+    res.render('addCat', { breeds });
 });
 
 app.get('/cats/add-breed', (req, res) => {
@@ -29,8 +23,10 @@ app.get('/cats/add-breed', (req, res) => {
 });
 
 app.post('/cats/add-breed', (req, res) => {
-    breeds.unshift(req.body.breed);
-    res.redirect('/cats/add-cat');
+    breeds.unshift({ breed: req.body.breed });
+    let jsonData = JSON.stringify(breeds);
+    fs.writeFile('breeds.json', jsonData, 'utf-8')
+        .then(() => res.redirect('/cats/add-cat'));
 });
 
 app.listen((port), () => console.log(`Server is working at port: ${port}`));
