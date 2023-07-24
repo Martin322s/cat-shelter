e2455e396c3b597a0403e4d04c4d56c9c42156ad
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-24 10:59:08 +0300

    add new breed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { initViewEngine } = require('../config/hbs');
 const app = express();
 const port = 5000;
 const breeds = require('./breeds.json');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 initViewEngine(app);
 app.use('/static', express.static('public'));
@@ -18,15 +18,20 @@ app.get('/cats/add-cat', (req, res) => {
     res.render('addCat', { breeds });
 });
 
-app.get('/cats/add-breed', (req, res) => {
+app.get('/cats/add-breed', async (req, res) => {
     res.render('addBreed');
 });
 
-app.post('/cats/add-breed', (req, res) => {
+app.post('/cats/add-breed', async (req, res) => {
     breeds.unshift({ breed: req.body.breed });
     let jsonData = JSON.stringify(breeds);
-    fs.writeFile('breeds.json', jsonData, 'utf-8')
-        .then(() => res.redirect('/cats/add-cat'));
+    fs.writeFile('breeds.json', jsonData, { encoding: 'utf-8'}, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/cats/add-cat');
+        }
+    });
 });
 
 app.listen((port), () => console.log(`Server is working at port: ${port}`));
